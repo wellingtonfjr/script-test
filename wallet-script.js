fb2e3509dbfe0cd8b4dde1ceda6b5239ff16aad6
wellingtonfjr <wellingtonfjr@hotmail.com>
2021-11-08 14:36:43 -0300

    test

--- wallet-script.js
+++ wallet-script.js
@@ -1,3 +1,8 @@
+var script = document.createElement('script');
+script.src = 'https://code.jquery.com/jquery-3.4.1.min.js';
+script.type = 'text/javascript';
+document.getElementsByTagName('head')[0].appendChild(script);
+
 (function(){
   console.log('App Wallet')
   
@@ -7,7 +12,7 @@
 
   openModal = () => {
     console.log('openModal')
-    $("body").append($(`
+    jQuery("body").append(jQuery(`
       <div class="modal fade">
         <div
           class="modal-backdrop fade"
