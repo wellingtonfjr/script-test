29215d5c47240ce695004b176210ac40dbf1f81e
wellingtonfjr <wellingtonfjr@hotmail.com>
2021-11-30 17:39:12 -0300

    adjust flux to execute functions

--- build/bundle.js
+++ build/bundle.js
@@ -8,13 +8,6 @@ script.src = 'https://code.jquery.com/jquery-3.4.1.min.js';
 script.type = 'text/javascript';
 document.getElementsByTagName('head')[0].appendChild(script);
 
-if (typeof window === 'object') {
-  console.log('entrou window script');
-  window.SDKCheckout.subscribeEvent('RENDER_NEXT', renderNext);
-  window.SDKCheckout.subscribeEvent('CLOSE_ORDER', closeOrder);
-  phoneLS = window.LS?.cart?.contact?.phone;
-}
-
 renderNext = () => {
   renderBoxOptinWallet();
 };
@@ -25,6 +18,13 @@ closeOrder = (event, data) => {
   console.log('Request validate Finish order: ', event, 'data: ', data);
   console.log('Cadastro do email: ', iAcceptWallet, 'e telefone: ', phoneWallet);
 };
+
+if (typeof window === 'object') {
+  console.log('entrou window script');
+  window.SDKCheckout.subscribeEvent('RENDER_NEXT', renderNext);
+  window.SDKCheckout.subscribeEvent('CLOSE_ORDER', closeOrder);
+  phoneLS = window.LS?.cart?.contact?.phone;
+}
 
 termsOfUse = e => {
   e.preventDefault();
