0c6ea2ee6008868a6df1edf8476bcf81f5102a7b
wellingtonfjr <wellingtonfjr@hotmail.com>
2021-12-01 15:53:57 -0300

    testing

--- build/bundle.js
+++ build/bundle.js
@@ -31,6 +31,7 @@ termsOfUse = e => {
   console.log('termsOfUse');
   openModal('Termos de uso', `
     <div class="col-12">
+      <b>Data da publicação: 20/10/2021</b>
       <p>
         Estes Termos e Condições de Uso da Plataforma Nuvemshop regulam a utilização do site “www.nuvemshop.com.br” e qualquer outro site, plataforma ou aplicativo operado pela Nuvemshop (“Plataforma”) e dos serviços disponibilizados na Plataforma (“Serviços”).
       </p>
@@ -57,7 +58,22 @@ privacyPolicy = e => {
   openModal('Política de Privacidade', `
     <div class="col-12">
       <p>
-        Privacidade
+        A Linked Store Brasil Hospedagem de Sites e Desenvolvimento de Softwares Ltda., pessoa jurídica com sede na Alameda Vicente Pinzon, 173, 8º Andar - Vila Olímpia , São Paulo - SP, Brasil, 04547-130, inscrita no CNPJ/MF sob o nº 016.932.748/0001-62 (“Nuvemshop” ou “nós”) levam a sua privacidade a sério e zela pela segurança e proteção de dados de todos os seus clientes, parceiros, fornecedores e usuários (“Usuários” ou “você”) do site “www.nuvemshop.com.br” e qualquer outro site, plataforma, aplicativo operado pela NUVEMSHOP (“Plataforma”).
+      </p>
+      <p>
+        Esta Política de Privacidade (“Política de Privacidade”) destina-se a informá-lo sobre o modo como nós utilizamos e divulgamos informações coletadas em suas visitas à nossa Plataforma e em mensagens que trocamos com você (“Comunicações”). Esta Política de Privacidade aplica-se somente a informações coletadas por meio da Plataforma.
+      </p>
+      <p>  
+        Ao acessar a plataforma, enviar comunicações ou fornecer qualquer tipo de dado pessoal, você entende os termos aqui previstos e aceita esta política de privacidade, consentindo com o tratamento de suas informações, conforme aqui descrito ou nos termos da legiislação em vigor.
+      </p>
+      <p>  
+        Esta Política de Privacidade fornece uma visão geral de nossas práticas de privacidade e das escolhas que você pode fazer, bem como direitos que você pode exercer em relação aos Dados Pessoais tratados por nós. Se você tiver alguma dúvida sobre o uso de Dados Pessoais, entre em contato pelo e-mail privacidade@nuvemshop.com.br.
+      </p>
+      <p>
+        Além disso, a Política de Privacidade não se aplica a quaisquer aplicativos, produtos, serviços, site ou recursos de mídia social de terceiros que possam ser oferecidos ou acessados por meio da Plataforma. O acesso a esses links fará com que você deixe a Plataforma e possa resultar na coleta ou compartilhamento de informações sobre você por terceiros. Nós não controlamos, endossamos ou fazemos quaisquer representações sobre esses sites de terceiros ou suas práticas de privacidade, que podem ser diferentes das nossas. Recomendamos que você revise a política de privacidade de qualquer site com o qual você interaja antes de permitir a coleta e o uso de seus Dados Pessoais.
+      </p>
+      <p>
+        Caso você nos envie Dados Pessoais referentes a outras pessoas físicas, você declara ter a competência para fazê-lo e declara ter obtido o consentimento necessário para autorizar o uso de tais informações nos termos desta Política de Privacidade.
       </p>
     </div>
   `);
@@ -66,7 +82,7 @@ privacyPolicy = e => {
 renderBoxOptinWallet = () => {
   console.log('**** Inside render box');
   jQuery("#boxOptInWallet").remove();
-  console.log('**** After remove box');
+  console.log('**** After remove box', jQuery("#btnFinishCheckout"));
   jQuery("#btnFinishCheckout").before(jQuery(` 
     <div class="panel panel-with-header"
       style="padding: 0"
@@ -104,7 +120,7 @@ renderBoxOptinWallet = () => {
         <div class="form-group">
           <input class="form-control" id="phoneWallet" type="text" placeholder="Celular com DDD" value="${phoneLS}" />
         </div>
-        <div>Você acessará esta conta através de um código recebido por SMS.</div>
+        <i style="margin-bottom: 15px; display: block">Você acessará esta conta através de um código recebido por SMS.</i>
         <div>Ao salvar, você aceita os <a onClick="termsOfUse(event)" href="#">Termos de uso</a> e <a onClick="privacyPolicy(event)" href="#">Política de privacidade</a>.</div>
       </div>
     </div>
