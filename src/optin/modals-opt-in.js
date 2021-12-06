1e17bf8d06d1ca32b9e2d288021b14365acd1337
wellingtonfjr <wellingtonfjr@hotmail.com>
2021-12-06 16:20:43 -0300

    externed functions build modal on optin

---
+++ src/optin/modals-opt-in.js
@@ -0,0 +1,52 @@
+export const termsOfUse = (e) => {
+  e.preventDefault()
+  openModal('Termos de uso', `
+    <div class="col-12">
+      <b class="m-top-half d-block">Data da publicação: 20/10/2021</b>
+      <p>
+        Estes Termos e Condições de Uso da Plataforma Nuvemshop regulam a utilização do site “www.nuvemshop.com.br” e qualquer outro site, plataforma ou aplicativo operado pela Nuvemshop (“Plataforma”) e dos serviços disponibilizados na Plataforma (“Serviços”).
+      </p>
+      <p>
+        A Plataforma é operada no Brasil pela Linked Store Brasil Hospedagem de Sites e Desenvolvimento de Softwares Ltda., pessoa jurídica com sede na Alameda Vicente Pinzon, 173, 8º Andar - Vila Olímpia, São Paulo/SP, Brasil, CEP 04547-130, inscrita no CNPJ/MF sob o nº 16.932.748/0001-62, (“Nuvemshop” ou “nós”).
+      <p>
+        Pedimos que os usuários leiam cuidadosamente e certifiquem-se de haver entendido e aceitado todas as condições estabelecidas nestes termos e nos demais documentos a eles incorporados por referência (em conjunto, "Termos"), uma vez que as regras, obrigações e direitos previstos vinculam e são válidos para todos os usuários da Plataforma (“Usuário” ou “você”). Em caso de dúvidas, o Usuário poderá enviar e-mail para contato@nuvemshop.com.br.
+      </p>
+      <p>  
+        Oferecemos serviços variados e, por vezes, termos adicionais podem ser aplicáveis. Ao utilizar um determinado Serviço, você estará sujeito às diretrizes, termos e condições a ele aplicáveis. Salvo se estabelecido de forma diversa, qualquer novo recurso que aumente ou aprimore os Serviços atualmente disponibilizados, incluindo novos lançamentos, estará automaticamente sujeito aos Termos.
+      </p>
+      <p>
+        Caso você não esteja de acordo com os Termos, solicitamos que não faça uso da Plataforma, tampouco efetue qualquer Cadastro ou envie dados, pessoais ou não.
+      </p>
+      <p>  
+        Durante a utilização da Plataforma, certos dados pessoais serão coletados e tratados pela Nuvemshop e/ou por nossos parceiros ("Parceiros"). As regras relacionadas ao tratamento de dados pessoais pela Nuvemshop estão estipuladas na Política de Privacidade.
+      </p>
+    </div>
+  `)
+}
+
+
+export const privacyPolicy = (e) => {
+  e.preventDefault()
+  openModal('Política de Privacidade', `
+    <div class="col-12">
+      <p>
+        A Linked Store Brasil Hospedagem de Sites e Desenvolvimento de Softwares Ltda., pessoa jurídica com sede na Alameda Vicente Pinzon, 173, 8º Andar - Vila Olímpia , São Paulo - SP, Brasil, 04547-130, inscrita no CNPJ/MF sob o nº 016.932.748/0001-62 (“Nuvemshop” ou “nós”) levam a sua privacidade a sério e zela pela segurança e proteção de dados de todos os seus clientes, parceiros, fornecedores e usuários (“Usuários” ou “você”) do site “www.nuvemshop.com.br” e qualquer outro site, plataforma, aplicativo operado pela NUVEMSHOP (“Plataforma”).
+      </p>
+      <p>
+        Esta Política de Privacidade (“Política de Privacidade”) destina-se a informá-lo sobre o modo como nós utilizamos e divulgamos informações coletadas em suas visitas à nossa Plataforma e em mensagens que trocamos com você (“Comunicações”). Esta Política de Privacidade aplica-se somente a informações coletadas por meio da Plataforma.
+      </p>
+      <p>  
+        Ao acessar a plataforma, enviar comunicações ou fornecer qualquer tipo de dado pessoal, você entende os termos aqui previstos e aceita esta política de privacidade, consentindo com o tratamento de suas informações, conforme aqui descrito ou nos termos da legiislação em vigor.
+      </p>
+      <p>  
+        Esta Política de Privacidade fornece uma visão geral de nossas práticas de privacidade e das escolhas que você pode fazer, bem como direitos que você pode exercer em relação aos Dados Pessoais tratados por nós. Se você tiver alguma dúvida sobre o uso de Dados Pessoais, entre em contato pelo e-mail privacidade@nuvemshop.com.br.
+      </p>
+      <p>
+        Além disso, a Política de Privacidade não se aplica a quaisquer aplicativos, produtos, serviços, site ou recursos de mídia social de terceiros que possam ser oferecidos ou acessados por meio da Plataforma. O acesso a esses links fará com que você deixe a Plataforma e possa resultar na coleta ou compartilhamento de informações sobre você por terceiros. Nós não controlamos, endossamos ou fazemos quaisquer representações sobre esses sites de terceiros ou suas práticas de privacidade, que podem ser diferentes das nossas. Recomendamos que você revise a política de privacidade de qualquer site com o qual você interaja antes de permitir a coleta e o uso de seus Dados Pessoais.
+      </p>
+      <p>
+        Caso você nos envie Dados Pessoais referentes a outras pessoas físicas, você declara ter a competência para fazê-lo e declara ter obtido o consentimento necessário para autorizar o uso de tais informações nos termos desta Política de Privacidade.
+      </p>
+    </div>
+  `)
+}
